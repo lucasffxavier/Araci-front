b0cd292b62393b977ba89690599edaa44f5f65d3
Xavier <lucasxavier.engenharia@gmail.com>
2026-05-28 13:07:55 -0300

    up

--- Properties/PropertiesHostView.xaml.cs
+++ Properties/PropertiesHostView.xaml.cs
@@ -1,18 +1,33 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace Araci.Properties
 {
-    public partial class PropertiesHostView
-        : UserControl
+    public partial class PropertiesHostView : UserControl
     {
-        // =========================
-        // CONSTRUTOR
-        // =========================
-
         public PropertiesHostView()
         {
             InitializeComponent();
+            AddHandler(Keyboard.PreviewKeyDownEvent, new KeyEventHandler(OnPreviewKeyDown), true);
+        }
 
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter)
+                return;
+
+            if (Keyboard.FocusedElement is not TextBox textBox)
+                return;
+
+            BindingExpression? binding = textBox.GetBindingExpression(TextBox.TextProperty);
+            binding?.UpdateSource();
+
+            FocusManager.SetFocusedElement(FocusManager.GetFocusScope(textBox), null);
+            Keyboard.ClearFocus();
+
+            e.Handled = true;
         }
     }
 }
