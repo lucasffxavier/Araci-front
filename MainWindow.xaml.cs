3c9266480a2f0b03e74cab13fd4486188b02f623
Xavier <lucasxavier.engenharia@gmail.com>
2026-05-29 12:28:20 -0300

    up

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Araci.Services;
-
 using System.Windows;
 using System.Windows.Controls.Primitives;
 
@@ -8,15 +7,13 @@ namespace Araci
     public partial class MainWindow : Window
     {
         private readonly EditorContext _context;
+        private readonly GridLength _propertiesColumnWidth = new(320);
 
         public MainWindow()
         {
             InitializeComponent();
-
             _context = new EditorContext();
-
             Viewport.Inicializar(_context);
-
             InicializarRibbon();
         }
 
@@ -26,8 +23,7 @@ namespace Araci
             ArquivoMenu.DataContext = _context;
         }
 
-        public void ToggleArquivoMenu(
-            UIElement referencia)
+        public void ToggleArquivoMenu(UIElement referencia)
         {
             if (ArquivoPopup.IsOpen)
             {
@@ -35,12 +31,8 @@ namespace Araci
                 return;
             }
 
-            ArquivoPopup.PlacementTarget =
-                referencia;
-
-            ArquivoPopup.Placement =
-                PlacementMode.Bottom;
-
+            ArquivoPopup.PlacementTarget = referencia;
+            ArquivoPopup.Placement = PlacementMode.Bottom;
             ArquivoPopup.IsOpen = true;
         }
 
@@ -48,6 +40,19 @@ namespace Araci
         {
             Viewport.Focus();
             System.Windows.Input.Keyboard.Focus(Viewport);
+        }
+
+        public void MostrarPropriedades()
+        {
+            PropertiesColumn.Width = _propertiesColumnWidth;
+            PropertiesHost.Visibility = Visibility.Visible;
+        }
+
+        private void OnPropertiesCloseRequested(object sender, RoutedEventArgs e)
+        {
+            PropertiesColumn.Width = new GridLength(0);
+            PropertiesHost.Visibility = Visibility.Collapsed;
+            FocarViewport();
         }
     }
 }
--- Properties/PropertiesHostView.xaml.cs
+++ Properties/PropertiesHostView.xaml.cs
@@ -7,10 +7,27 @@ namespace Araci.Properties
 {
     public partial class PropertiesHostView : UserControl
     {
+        public static readonly RoutedEvent CloseRequestedEvent = EventManager.RegisterRoutedEvent(
+            nameof(CloseRequested),
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(PropertiesHostView));
+
+        public event RoutedEventHandler CloseRequested
+        {
+            add => AddHandler(CloseRequestedEvent, value);
+            remove => RemoveHandler(CloseRequestedEvent, value);
+        }
+
         public PropertiesHostView()
         {
             InitializeComponent();
             AddHandler(Keyboard.PreviewKeyDownEvent, new KeyEventHandler(OnPreviewKeyDown), true);
+        }
+
+        private void OnCloseClick(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(CloseRequestedEvent));
         }
 
         private void OnPreviewKeyDown(object sender, KeyEventArgs e)
@@ -23,10 +40,8 @@ namespace Araci.Properties
 
             BindingExpression? binding = textBox.GetBindingExpression(TextBox.TextProperty);
             binding?.UpdateSource();
-
             FocusManager.SetFocusedElement(FocusManager.GetFocusScope(textBox), null);
             Keyboard.ClearFocus();
-
             e.Handled = true;
         }
     }
