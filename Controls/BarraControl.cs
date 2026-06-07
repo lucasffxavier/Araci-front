236773571692c66862e44697f70720422c55f473
lucasffxavier <lucasxavier.engenharia@gmail.com>
2026-06-07 19:01:01 -0300

    up

--- Controls/BarraControl.cs
+++ Controls/BarraControl.cs
@@ -1,7 +1,5 @@
 ﻿using Araci.Controls.Base;
 using Araci.Controls.Converters;
-using SharpVectors.Converters;
-using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,8 +9,9 @@ namespace Araci.Controls
 {
     public class BarraControl : ElementoControlBase
     {
+        private const double HandleSize = 12;
         private readonly Grid _root;
-        private readonly SvgViewbox _svg;
+        private readonly Border _body;
         private readonly Border _overlay;
         private readonly Border _previewOverlay;
         private readonly Border _topHandle;
@@ -21,16 +20,17 @@ namespace Araci.Controls
         public BarraControl()
         {
             Cursor = System.Windows.Input.Cursors.Hand;
-            _svg = new SvgViewbox
+            _body = new Border
             {
-                Stretch = Stretch.Fill,
-                Source = new Uri("pack://application:,,,/Resources/Svg/barra.svg")
+                Background = Brushes.Black,
+                IsHitTestVisible = false
             };
             _overlay = new Border
             {
                 Background = Brushes.DeepSkyBlue,
                 Opacity = 0.6,
-                Visibility = Visibility.Collapsed
+                Visibility = Visibility.Collapsed,
+                IsHitTestVisible = false
             };
             _previewOverlay = new Border
             {
@@ -42,31 +42,26 @@ namespace Araci.Controls
             _topHandle = CriarHandle(VerticalAlignment.Top);
             _bottomHandle = CriarHandle(VerticalAlignment.Bottom);
             _root = new Grid { ClipToBounds = false };
-            _root.Children.Add(_svg);
+            _root.Children.Add(_body);
             _root.Children.Add(_previewOverlay);
             _root.Children.Add(_overlay);
             _root.Children.Add(_topHandle);
             _root.Children.Add(_bottomHandle);
             Content = _root;
-            Loaded += OnLoaded;
             ConfigurarBindings();
         }
 
         protected override bool UsaBindings => true;
 
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-            _overlay.OpacityMask = new VisualBrush(_svg);
-            _previewOverlay.OpacityMask = new VisualBrush(_svg);
-        }
-
         private static Border CriarHandle(VerticalAlignment verticalAlignment)
         {
+            double handleOffset = -HandleSize / 2;
+
             return new Border
             {
-                Width = 12,
-                Height = 12,
-                CornerRadius = new CornerRadius(6),
+                Width = HandleSize,
+                Height = HandleSize,
+                CornerRadius = new CornerRadius(HandleSize / 2),
                 BorderThickness = new Thickness(2),
                 BorderBrush = Brushes.White,
                 Background = Brushes.DeepSkyBlue,
@@ -74,7 +69,7 @@ namespace Araci.Controls
                 VerticalAlignment = verticalAlignment,
                 Visibility = Visibility.Collapsed,
                 IsHitTestVisible = false,
-                Margin = verticalAlignment == VerticalAlignment.Top ? new Thickness(0, -6, 0, 0) : new Thickness(0, 0, 0, -6)
+                Margin = verticalAlignment == VerticalAlignment.Top ? new Thickness(0, handleOffset, 0, 0) : new Thickness(0, 0, 0, handleOffset)
             };
         }
 
@@ -82,12 +77,14 @@ namespace Araci.Controls
         {
             SetBinding(WidthProperty, new Binding("RenderData.Largura"));
             SetBinding(HeightProperty, new Binding("RenderData.Altura"));
-            _svg.SetBinding(WidthProperty, new Binding("RenderData.Largura"));
-            _svg.SetBinding(HeightProperty, new Binding("RenderData.Altura"));
-            _svg.SetBinding(OpacityProperty, new Binding("IsPreview") { Converter = new BoolToOpacityConverter() });
+            _body.SetBinding(WidthProperty, new Binding("RenderData.Largura"));
+            _body.SetBinding(HeightProperty, new Binding("RenderData.Altura"));
+            _body.SetBinding(OpacityProperty, new Binding("IsPreview") { Converter = new BoolToOpacityConverter() });
             _previewOverlay.SetBinding(WidthProperty, new Binding("RenderData.Largura"));
             _previewOverlay.SetBinding(HeightProperty, new Binding("RenderData.Altura"));
             _previewOverlay.SetBinding(VisibilityProperty, new Binding("IsPreview") { Converter = new BooleanToVisibilityConverter() });
+            _overlay.SetBinding(WidthProperty, new Binding("RenderData.Largura"));
+            _overlay.SetBinding(HeightProperty, new Binding("RenderData.Altura"));
             var multi = new MultiBinding { Converter = new SelectionOrHoverToVisibilityConverter() };
             multi.Bindings.Add(new Binding("IsSelecionado"));
             multi.Bindings.Add(new Binding("IsHover"));
