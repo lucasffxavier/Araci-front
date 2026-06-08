7f4f58120fc81ebfce475e252d25004ffa7f3c4f
lucasffxavier <lucasxavier.engenharia@gmail.com>
2026-06-08 09:50:31 -0300

    up

--- ViewModels/ProjectBrowserViewModel.cs
+++ ViewModels/ProjectBrowserViewModel.cs
@@ -1,14 +1,21 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using Araci.Core.Documents;
+using Araci.ViewModels.Base;
 
 namespace Araci.ViewModels
 {
     public sealed class ProjectBrowserViewModel
     {
         private readonly AraciDocument _document;
+        private Guid? _selectedItemId;
+        private string? _selectedItemKind;
 
         public ProjectBrowserViewModel(AraciDocument document)
         {
@@ -36,16 +43,68 @@ namespace Araci.ViewModels
 
         private void AtualizarSecoes()
         {
-            AtualizarItens(Secoes[0], _document.Vistas.Select(v => v.Nome));
-            AtualizarItens(Secoes[1], _document.Tabelas.Select(t => t.Nome).DefaultIfEmpty("Nenhuma tabela"));
-            AtualizarItens(Secoes[2], _document.Pranchas.Select(p => FormatarPrancha(p)).DefaultIfEmpty("Nenhuma prancha"));
+            AtualizarItens(Secoes[0], _document.Vistas.Select(v => CriarItem(v.Id, "Vista", v.Nome)));
+            AtualizarItens(Secoes[1], CriarItensTabela());
+            AtualizarItens(Secoes[2], CriarItensPrancha());
         }
 
-        private static void AtualizarItens(ProjectBrowserSectionViewModel secao, IEnumerable<string> itens)
+        private IEnumerable<ProjectBrowserItemViewModel> CriarItensTabela()
+        {
+            if (_document.Tabelas.Count == 0)
+            {
+                yield return ProjectBrowserItemViewModel.CriarPlaceholder("Nenhuma tabela");
+                yield break;
+            }
+
+            foreach (ProjectTable tabela in _document.Tabelas)
+                yield return CriarItem(tabela.Id, "Tabela", tabela.Nome);
+        }
+
+        private IEnumerable<ProjectBrowserItemViewModel> CriarItensPrancha()
+        {
+            if (_document.Pranchas.Count == 0)
+            {
+                yield return ProjectBrowserItemViewModel.CriarPlaceholder("Nenhuma prancha");
+                yield break;
+            }
+
+            foreach (ProjectSheet prancha in _document.Pranchas)
+                yield return CriarItem(prancha.Id, "Prancha", FormatarPrancha(prancha));
+        }
+
+        private ProjectBrowserItemViewModel CriarItem(Guid id, string tipo, string nome)
+        {
+            var item = new ProjectBrowserItemViewModel(
+                id,
+                tipo,
+                nome,
+                true,
+                SelecionarItem);
+
+            item.IsSelected = _selectedItemId == id && _selectedItemKind == tipo;
+            return item;
+        }
+
+        private void SelecionarItem(ProjectBrowserItemViewModel item)
+        {
+            if (!item.IsSelectable)
+                return;
+
+            _selectedItemId = item.Id;
+            _selectedItemKind = item.Tipo;
+
+            foreach (ProjectBrowserSectionViewModel secao in Secoes)
+            {
+                foreach (ProjectBrowserItemViewModel atual in secao.Itens)
+                    atual.IsSelected = ReferenceEquals(atual, item);
+            }
+        }
+
+        private static void AtualizarItens(ProjectBrowserSectionViewModel secao, IEnumerable<ProjectBrowserItemViewModel> itens)
         {
             secao.Itens.Clear();
 
-            foreach (string item in itens)
+            foreach (ProjectBrowserItemViewModel item in itens)
                 secao.Itens.Add(item);
         }
 
@@ -62,10 +121,62 @@ namespace Araci.ViewModels
         public ProjectBrowserSectionViewModel(string titulo)
         {
             Titulo = titulo;
-            Itens = new ObservableCollection<string>();
+            Itens = new ObservableCollection<ProjectBrowserItemViewModel>();
         }
 
         public string Titulo { get; }
-        public ObservableCollection<string> Itens { get; }
+        public ObservableCollection<ProjectBrowserItemViewModel> Itens { get; }
+    }
+
+    public sealed class ProjectBrowserItemViewModel : INotifyPropertyChanged
+    {
+        private readonly Action<ProjectBrowserItemViewModel>? _selecionar;
+        private bool _isSelected;
+
+        public ProjectBrowserItemViewModel(Guid id, string tipo, string nome, bool isSelectable, Action<ProjectBrowserItemViewModel>? selecionar)
+        {
+            Id = id;
+            Tipo = tipo;
+            Nome = nome;
+            IsSelectable = isSelectable;
+            _selecionar = selecionar;
+            SelecionarCommand = new RelayCommand(Selecionar, () => IsSelectable);
+        }
+
+        public Guid Id { get; }
+        public string Tipo { get; }
+        public string Nome { get; }
+        public bool IsSelectable { get; }
+        public ICommand SelecionarCommand { get; }
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected == value)
+                    return;
+
+                _isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public static ProjectBrowserItemViewModel CriarPlaceholder(string nome)
+        {
+            return new ProjectBrowserItemViewModel(Guid.Empty, "Placeholder", nome, false, null);
+        }
+
+        private void Selecionar()
+        {
+            _selecionar?.Invoke(this);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
