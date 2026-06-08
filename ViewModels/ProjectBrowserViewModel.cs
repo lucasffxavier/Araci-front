b95654ea979a49e00bb103b6b2e8bd9f8dc9b28a
lucasffxavier <lucasxavier.engenharia@gmail.com>
2026-06-08 08:51:21 -0300

    up

--- ViewModels/ProjectBrowserViewModel.cs
+++ ViewModels/ProjectBrowserViewModel.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.ObjectModel;
 
 namespace Araci.ViewModels
@@ -9,9 +8,9 @@ namespace Araci.ViewModels
         {
             Secoes = new ObservableCollection<ProjectBrowserSectionViewModel>
             {
-                new("Vistas", new Uri("pack://application:,,,/Resources/Icons/vistas.svg"), "Vista principal"),
-                new("Tabelas", new Uri("pack://application:,,,/Resources/Icons/tabela.svg"), "Nenhuma tabela"),
-                new("Pranchas", new Uri("pack://application:,,,/Resources/Icons/prancha.svg"), "Nenhuma prancha")
+                new("Vistas", "Vista principal"),
+                new("Tabelas", "Nenhuma tabela"),
+                new("Pranchas", "Nenhuma prancha")
             };
         }
 
@@ -20,15 +19,13 @@ namespace Araci.ViewModels
 
     public sealed class ProjectBrowserSectionViewModel
     {
-        public ProjectBrowserSectionViewModel(string titulo, Uri icone, string itemVisual)
+        public ProjectBrowserSectionViewModel(string titulo, string itemVisual)
         {
             Titulo = titulo;
-            Icone = icone;
             Itens = new ObservableCollection<string> { itemVisual };
         }
 
         public string Titulo { get; }
-        public Uri Icone { get; }
         public ObservableCollection<string> Itens { get; }
     }
 }
